fcc69c1c9607196372f741b74dbfe09e9c9a305d
Bruno Constantino <161530884+bbruno8@users.noreply.github.com>
2025-04-13 15:08:21 -0300

    exercicio 10 concluido com sucesso, arquivos dos exercicios 11 e 12 adicionados

---
+++ src/etapa2/C08EX12.java
@@ -0,0 +1,11 @@
+package etapa2;
+
+import java.util.Scanner;
+
+public class C08EX12 {
+    //Bruno Constantino Gomes da Costa
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+    }
+}
